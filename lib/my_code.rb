5480968a60a56c8db6019a6dd7e6a24ce89c8732
Catherine O'Hara <catherine.ohara@outlook.com>
2019-06-22 17:14:44 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,23 +45,35 @@ new_array
 end 
 
 
-def reduce_to_total(source_array, starting_point)
-  total = 0 
+def reduce_to_total(source_array, starting_point=0)
+  total = starting_point
   i = 0
   
   while i < source_array.length do
     total = total + source_array[i]
     i += 1
   end
-total + starting_point
+total
 end
 
 
 def reduce_to_all_true(source_array)
+  i = 0 
   
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end 
+return true
 end
 
 
 def reduce_to_any_true(source_array)
+  i = 0 
   
+  while i < source_array.length do
+    return true if source_array[i]
+    i += 1
+  end 
+return false
 end
